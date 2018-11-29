8bdd1bb6629dd5eddb13c7f797af908a600fd622
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2018-11-29 12:32:25 -0300

    Estructura de styles

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,21 +1,12 @@
 import React, {Component} from 'react'
-import {StyleSheet, Text, View, Button } from 'react-native'
-
-const styles = StyleSheet.create({
-    //Estilo de la pantalla de bienvenida
-    welcomeView: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+import { Text, View, Button } from 'react-native'
+import welcomeStyles from '../styles/WelcomeStyles'
   
 //Pantalla de bienvenida y carga de JSON
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View style={styles.welcomeView}>
+      <View style={welcomeStyles.welcomeView}>
         <Text>
           Bienvenido
         </Text>
---
+++ src/styles/WelcomeStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const welcomeStyles = StyleSheet.create({
+    //Estilo de la pantalla de bienvenida
+    welcomeView: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default welcomeStyles
