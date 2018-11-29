a14bce83f6e3a671ed313a038e09b7ee5d04ff61
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2018-11-29 13:24:46 -0300

    Agregada imagen logo

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Text, View, Button } from 'react-native'
+import { Image,Text, View, Button } from 'react-native'
 import welcomeStyles from '../styles/WelcomeStyles'
   
 //Pantalla de bienvenida y carga de JSON
@@ -8,8 +8,9 @@ class WelcomeScreen extends Component {
     return (
       <View style={welcomeStyles.welcomeView}>
         <Text>
-          Bienvenido
+          Bienvenidos a Resuelvo Explorando!
         </Text>
+        <Image source={require('../../assets/images/resuelvo_explorando_logo.png')}/>
         <Button
           onPress={() => this.props.navigation.push('Main')}
           title='Comenzar'
