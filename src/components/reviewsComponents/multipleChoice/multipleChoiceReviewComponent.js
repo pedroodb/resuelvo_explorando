95ccd5af9836941601f2c14bbf36736791fa4c7d
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2019-02-08 11:53:45 -0300

    Eliminados imports inutiles

--- src/components/reviewsComponents/multipleChoice/multipleChoiceReviewComponent.js
+++ src/components/reviewsComponents/multipleChoice/multipleChoiceReviewComponent.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 
 import { DefaultButton } from '../../../components'
-import CheckBoxWText from './checkBoxWText'
 import { viewStyle } from '../../styles/genericStyles'
 
 //Componente que corresponde a una tarea del tipo multiple choice
