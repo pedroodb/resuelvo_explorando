249b6e643867630ec5748e68c84afeaa477fa887
pedroodb <dalbianco.pedro@gmail.com>
2019-02-20 00:11:06 -0300

    Refactoring de estilos

--- src/containers/FinalReviewScreen.js
+++ src/containers/FinalReviewScreen.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
-import { Text, View, Button, StyleSheet } from 'react-native'
+import { Text, View } from 'react-native'
+
+import { 
+  viewStyle,
+  resultStyle,
+  titleStyle,
+ } from '../styles/FinalReviewStyles'
 import { DefaultButton } from '../components'
 
 class FinalReviewScreenComponent extends Component {
@@ -22,37 +28,20 @@ class FinalReviewScreenComponent extends Component {
     } = this.props.navigation.getParam('educationalActivity')
 
     return (
-      <View style={styles.finalReviewScreen}>
-      <View style={{flex:1}}>
-        <Text style={styles.title}>Tu desempeño es:</Text>
-      </View>
-      <View style={{flex:3}}>
-        <Text style={styles.result}>{`${tasks.length} tareas sin resolver`}</Text>
-        <Text style={styles.result}>{`${(finishedTasks.filter(task => task.answer.isCorrect)).length} correctamente`}</Text>
-        <Text style={styles.result}>{`${(finishedTasks.filter(task => !task.answer.isCorrect)).length} incorrectamente`}</Text>
-      </View>
-      <DefaultButton title="Finalizar" onPress={() => this.props.navigation.popToTop()}/>
+      <View style={viewStyle}>
+        <View style={{flex:1}}>
+          <Text style={titleStyle}>Tu desempeño es:</Text>
+        </View>
+        <View style={{flex:3}}>
+          <Text style={resultStyle}>{`${tasks.length} tareas sin resolver`}</Text>
+          <Text style={resultStyle}>{`${(finishedTasks.filter(task => task.answer.isCorrect)).length} correctamente`}</Text>
+          <Text style={resultStyle}>{`${(finishedTasks.filter(task => !task.answer.isCorrect)).length} incorrectamente`}</Text>
+        </View>
+        <DefaultButton title="Finalizar" onPress={() => this.props.navigation.popToTop()}/>
       </View>
     )
   }
 
 }
-
-const styles = StyleSheet.create({
-  title: {
-    textAlign:'center',
-    fontSize:40,
-  },
-  result:{
-    fontSize:20,
-    marginLeft:20,
-    margin:10,
-  },
-  finalReviewScreen:{
-    flex:1,
-    justifyContent:'space-around',
-  },
-})
-
 
 export default FinalReviewScreenComponent
