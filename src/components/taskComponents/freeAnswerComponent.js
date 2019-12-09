320ecfa7d9a8ecba8c7d95485a8aacf099c246cb
pedroodb <dalbianco.pedro@gmail.com>
2019-12-09 12:02:33 -0300

    Vista derespuesta libre

--- src/components/taskComponents/freeAnswerComponent.js
+++ src/components/taskComponents/freeAnswerComponent.js
@@ -1,11 +1,12 @@
-//Aqui va componente para tarea de respuesta abierta
 import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
 import {
   TextInput,
-  Button,
   View,
   Text,
+  KeyboardAvoidingView,
 } from 'react-native'
+import DefaultButton from '../defaultButton';
 
 class FreeAnswerComponent extends Component {
 
@@ -35,29 +36,55 @@ class FreeAnswerComponent extends Component {
     } = this.state
 
     return (
-      <View>
-        <Text>{name}</Text>
-        <Text>{description}</Text>
-        <TextInput
-          onChangeText={this.updateAnswer}
-          value={answer}
-        />
-        {
-          answer ?
-            <Button
-              title='Guardar'
-              onPress={() => {
-                this.props.solveTask(this.props.task,this.state.answer)
-                this.props.navigation.navigate('TaskReview')
-              }}
-            />
-          : null
-        }
+      <View style={styles.viewFAC}>
+        <Text style={styles.titleFAC}>{name}</Text>
+        <Text style={styles.descriptionFAC}>{description}</Text>
+        <KeyboardAvoidingView behavior="padding">
+          <TextInput
+            onChangeText={this.updateAnswer}
+            value={answer}
+            placeholder='Ingrese aquí su respuesta'
+            autoFocus={true}
+            multiline={true}
+            style={styles.inputFAC}
+          />
+          {
+            answer ?
+              <DefaultButton
+                title='Guardar'
+                onPress={() => {
+                  this.props.solveTask(this.props.task,this.state.answer)
+                  this.props.navigation.navigate('TaskReview')
+                }}
+              />
+            : null
+          }
+        </KeyboardAvoidingView>
       </View>
     )
 
   }
 
 }
+
+const styles = StyleSheet.create({
+  viewFAC:{
+    flex:1,
+  },
+  titleFAC: {
+    textAlign:'center',
+    fontSize:30,
+  },
+  descriptionFAC: {
+    textAlign:'center',
+    fontSize:20,
+  },
+  inputFAC: {
+    margin:10,
+    textAlign:'center',
+    fontSize:20,
+  },
+})
+
 
 export default FreeAnswerComponent
