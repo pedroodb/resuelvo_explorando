dce2fbaa9ad828f31f65408940b700df7282f0de
pedroodb <dalbianco.pedro@gmail.com>
2019-12-15 18:32:50 -0300

    Se agregan comentarios

--- src/actions/activityActions.js
+++ src/actions/activityActions.js
@@ -3,11 +3,13 @@ import {
   TASK_SOLVED,
 } from '../constants/activityConstants'
 
+// Indica que se establecio una actividad en el estado de redux
 export const setActivity = activity => ({
   type: ACTIVITY_SET,
   payload: activity,
 })
 
+// Indica que se resolvio la tarea 'task'
 export const solveTask = (task, answer) => ({
   type: TASK_SOLVED,
   payload: {
--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -5,20 +5,24 @@ import {
   FINISHED_TASK_SET,
 } from '../constants/taskConstants'
 
+// Indica que se leyo un codigo de tarea
 export const setCode = code => ({
   type: CODE_SET,
   payload: code,
 })
 
+// Reestablece el valor del codigo de tarea leido
 export const resetCode = () => ({
   type: CODE_RESET,
 })
 
+// Indica que se establecio una tarea actual en el estado de redux
 export const setTask = task => ({
   type: TASK_SET,
   payload: task,
 })
 
+// Indica que se establecio una tarea finalizada en el estado de redux
 export const setFinishedTask = task => ({
   type: FINISHED_TASK_SET,
   payload: task,
